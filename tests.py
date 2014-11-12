ab150c7d5ee554f57427eaa62e56f37a9fb5b0ae
Robin Schroer <sulami@peerwire.org>
2014-11-12 20:30:36 +0100

    add more tests

--- tests.py
+++ tests.py
@@ -15,9 +15,28 @@ class TimingTestCase(TestCase):
         self.assertIn('0.00', t.output()) # This should run fast
 
     def test_multiple_functions(self):
-        t = Timer([a, b])
+        t = Timer([a, b]) # List
         t.run()
-        self.assertIn('0.00', t.output()) # This should run fast
+        self.assertIn('0.00', t.output())
+
+        t = Timer((a, b)) # Tuple
+        t.run()
+        self.assertIn('0.00', t.output())
+
+    def test_setup_function(self):
+        t = Timer(a, setup=b)
+        t.run()
+        self.assertIn('0.00', t.output())
+
+    def test_exit_function(self):
+        t = Timer(a, exit=b)
+        t.run()
+        self.assertIn('0.00', t.output())
+
+    def test_setup_exit(self):
+        t = Timer(a, setup=b, exit=b)
+        t.run()
+        self.assertIn('0.00', t.output())
 
 if __name__ == '__main__':
     main()
